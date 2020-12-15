ae997d54791c6af15c3e03ad0a467b462c3c99a3
jswelsh <james.sinclair.welsh@gmail.com>
2020-12-15 00:12:39 -0800

    adding typescript set up

--- script.js
+++ script.js
@@ -1,116 +1,117 @@
-am4core.ready(function() {
-  
-  // Themes begin
-  am4core.useTheme(am4themes_dark);
-  am4core.useTheme(am4themes_animated);
-  // Themes end
-  
-  
-  
-  var chart = am4core.create("chartdiv", am4plugins_forceDirected.ForceDirectedTree);
-  var networkSeries = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries())
-  
-  chart.data = [
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const am4core = __importStar(require("@amcharts/amcharts4/core"));
+const am4plugins_forceDirected = __importStar(require("@amcharts/amcharts4/plugins/forceDirected"));
+const dark_1 = __importDefault(require("@amcharts/amcharts4/themes/dark"));
+const animated_1 = __importDefault(require("@amcharts/amcharts4/themes/animated"));
+/* Chart code */
+// Themes begin
+am4core.useTheme(dark_1.default);
+am4core.useTheme(animated_1.default);
+// Themes end
+let chart = am4core.create("chartdiv", am4plugins_forceDirected.ForceDirectedTree);
+let networkSeries = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries());
+chart.data = [
     {
-      name: "Core",
-      children: [
-        {
-          name: "First",
-          children: [
-            { name: "A1", value: 100 },
-            { name: "A2", value: 60 }
-          ]
-        },
-        {
-          name: "Second",
-          children: [
-            { name: "B1", value: 135 },
-            { name: "B2", value: 98 }
-          ]
-        },
-        {
-          name: "Third",
-          children: [
+        name: "Core",
+        children: [
             {
-              name: "C1",
-              children: [
-                { name: "EE1", value: 130 },
-                { name: "EE2", value: 87 },
-                { name: "EE3", value: 55 }
-              ]
-            },
-            { name: "C2", value: 148 },
-            {
-              name: "C3", children: [
-                { name: "CC1", value: 53 },
-                { name: "CC2", value: 30 }
-              ]
-            },
-            { name: "C4", value: 26 }
-          ]
-        },
-        {
-          name: "Fourth",
-          children: [
-            { name: "D1", value: 415 },
-            { name: "D2", value: 148 },
-            { name: "D3", value: 89 }
-          ]
-        },
-        {
-          name: "Fifth",
-          children: [
-            {
-              name: "E1",
-              children: [
-                { name: "EE1", value: 33 },
-                { name: "EE2", value: 40 },
-                { name: "EE3", value: 89 }
-              ]
+                name: "First",
+                children: [
+                    { name: "A1", value: 100 },
+                    { name: "A2", value: 60 }
+                ]
             },
             {
-              name: "E2",
-              value: 148
+                name: "Second",
+                children: [
+                    { name: "B1", value: 135 },
+                    { name: "B2", value: 98 }
+                ]
+            },
+            {
+                name: "Third",
+                children: [
+                    {
+                        name: "C1",
+                        children: [
+                            { name: "EE1", value: 130 },
+                            { name: "EE2", value: 87 },
+                            { name: "EE3", value: 55 }
+                        ]
+                    },
+                    { name: "C2", value: 148 },
+                    {
+                        name: "C3", children: [
+                            { name: "CC1", value: 53 },
+                            { name: "CC2", value: 30 }
+                        ]
+                    },
+                    { name: "C4", value: 26 }
+                ]
+            },
+            {
+                name: "Fourth",
+                children: [
+                    { name: "D1", value: 415 },
+                    { name: "D2", value: 148 },
+                    { name: "D3", value: 89 }
+                ]
+            },
+            {
+                name: "Fifth",
+                children: [
+                    {
+                        name: "E1",
+                        children: [
+                            { name: "EE1", value: 33 },
+                            { name: "EE2", value: 40 },
+                            { name: "EE3", value: 89 }
+                        ]
+                    },
+                    {
+                        name: "E2",
+                        value: 148
+                    }
+                ]
             }
-          ]
-        }
-  
-      ]
+        ]
     }
-  ];
-  
-  networkSeries.dataFields.value = "value";
-  networkSeries.dataFields.name = "name";
-  networkSeries.dataFields.children = "children";
-  networkSeries.nodes.template.tooltipText = "{name}:{value}";
-  networkSeries.nodes.template.fillOpacity = 1;
-  
-  networkSeries.nodes.template.label.text = "{name}"
-  networkSeries.fontSize = 10;
-  
-  networkSeries.links.template.strokeWidth = 1;
-  
-  var hoverState = networkSeries.links.template.states.create("hover");
-  hoverState.properties.strokeWidth = 3;
-  hoverState.properties.strokeOpacity = 1;
-  
-  networkSeries.nodes.template.events.on("over", function(event) {
-    event.target.dataItem.childLinks.each(function(link) {
-      link.isHover = true;
-    })
+];
+networkSeries.dataFields.value = "value";
+networkSeries.dataFields.name = "name";
+networkSeries.dataFields.children = "children";
+networkSeries.nodes.template.tooltipText = "{name}:{value}";
+networkSeries.nodes.template.fillOpacity = 1;
+networkSeries.nodes.template.label.text = "{name}";
+networkSeries.fontSize = 10;
+networkSeries.links.template.strokeWidth = 1;
+let hoverState = networkSeries.links.template.states.create("hover");
+hoverState.properties.strokeWidth = 3;
+hoverState.properties.strokeOpacity = 1;
+networkSeries.nodes.template.events.on("over", function (event) {
+    event.target.dataItem.childLinks.each(function (link) {
+        link.isHover = true;
+    });
     if (event.target.dataItem.parentLink) {
-      event.target.dataItem.parentLink.isHover = true;
+        event.target.dataItem.parentLink.isHover = true;
     }
-  
-  })
-  
-  networkSeries.nodes.template.events.on("out", function(event) {
-    event.target.dataItem.childLinks.each(function(link) {
-      link.isHover = false;
-    })
+});
+networkSeries.nodes.template.events.on("out", function (event) {
+    event.target.dataItem.childLinks.each(function (link) {
+        link.isHover = false;
+    });
     if (event.target.dataItem.parentLink) {
-      event.target.dataItem.parentLink.isHover = false;
+        event.target.dataItem.parentLink.isHover = false;
     }
-  })
-  
-  }); // end am4core.ready()
+});
---
+++ script.ts
@@ -0,0 +1,122 @@
+import * as am4core from "@amcharts/amcharts4/core";
+
+// @ts-ignore
+import * as am4charts from "@amcharts/amcharts4/charts";
+import * as am4plugins_forceDirected from "@amcharts/amcharts4/plugins/forceDirected";
+
+import am4themes_dark from "@amcharts/amcharts4/themes/dark";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+/* Chart code */
+// Themes begin
+am4core.useTheme(am4themes_dark);
+am4core.useTheme(am4themes_animated);
+// Themes end
+
+
+
+let chart = am4core.create("chartdiv", am4plugins_forceDirected.ForceDirectedTree);
+let networkSeries = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries())
+
+chart.data = [
+  {
+    name: "Core",
+    children: [
+      {
+        name: "First",
+        children: [
+          { name: "A1", value: 100 },
+          { name: "A2", value: 60 }
+        ]
+      },
+      {
+        name: "Second",
+        children: [
+          { name: "B1", value: 135 },
+          { name: "B2", value: 98 }
+        ]
+      },
+      {
+        name: "Third",
+        children: [
+          {
+            name: "C1",
+            children: [
+              { name: "EE1", value: 130 },
+              { name: "EE2", value: 87 },
+              { name: "EE3", value: 55 }
+            ]
+          },
+          { name: "C2", value: 148 },
+          {
+            name: "C3", children: [
+              { name: "CC1", value: 53 },
+              { name: "CC2", value: 30 }
+            ]
+          },
+          { name: "C4", value: 26 }
+        ]
+      },
+      {
+        name: "Fourth",
+        children: [
+          { name: "D1", value: 415 },
+          { name: "D2", value: 148 },
+          { name: "D3", value: 89 }
+        ]
+      },
+      {
+        name: "Fifth",
+        children: [
+          {
+            name: "E1",
+            children: [
+              { name: "EE1", value: 33 },
+              { name: "EE2", value: 40 },
+              { name: "EE3", value: 89 }
+            ]
+          },
+          {
+            name: "E2",
+            value: 148
+          }
+        ]
+      }
+
+    ]
+  }
+];
+
+networkSeries.dataFields.value = "value";
+networkSeries.dataFields.name = "name";
+networkSeries.dataFields.children = "children";
+networkSeries.nodes.template.tooltipText = "{name}:{value}";
+networkSeries.nodes.template.fillOpacity = 1;
+
+networkSeries.nodes.template.label.text = "{name}"
+networkSeries.fontSize = 10;
+
+networkSeries.links.template.strokeWidth = 1;
+
+let hoverState = networkSeries.links.template.states.create("hover");
+hoverState.properties.strokeWidth = 3;
+hoverState.properties.strokeOpacity = 1;
+
+networkSeries.nodes.template.events.on("over", function(event) {
+  event.target.dataItem.childLinks.each(function(link) {
+    link.isHover = true;
+  })
+  if (event.target.dataItem.parentLink) {
+    event.target.dataItem.parentLink.isHover = true;
+  }
+
+})
+
+networkSeries.nodes.template.events.on("out", function(event) {
+  event.target.dataItem.childLinks.each(function(link) {
+    link.isHover = false;
+  })
+  if (event.target.dataItem.parentLink) {
+    event.target.dataItem.parentLink.isHover = false;
+  }
+})
