8f0e98e4669e8be0c021e7a8ed96637e56e28b2d
jswelsh <james.sinclair.welsh@gmail.com>
2020-12-23 12:32:45 -0800

    add: setting up linking to all applets within project

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,22 +2,28 @@ import Page from 'material-ui-shell/lib/containers/Page'
 import React from 'react'
 import Scrollbar from 'material-ui-shell/lib/components/Scrollbar/Scrollbar'
 import { useIntl } from 'react-intl'
-
-
+import BarChartIcon from '@material-ui/icons/BarChart';
+import MapIcon from '@material-ui/icons/Map';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
+import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
+
+import { 
+  Paper,
+  List,
+  Link,
+  ListItem,
+  Divider,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Typography, } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
       width: '100%',
-      maxWidth: '36ch',
+      margin: 'auto',
+      maxWidth: '600px',
       backgroundColor: theme.palette.background.paper,
     },
     inline: {
@@ -25,75 +31,73 @@ const useStyles = makeStyles((theme) =>
     },
   }),
 );
-
-function AlignItemsList() {
+const applets = [{
+  title: 'Pitch Path',
+  subHeader: 'A rugby pitch locator',
+  info: '',
+  link: '/pitch_path',
+  icon: <MapIcon/>
+},{
+  title: 'Technologies Graph',
+  subHeader: 'An interactive Node graph of the Dev-Tech I use',
+  info: 'Click the circles to toggle show/hide functionality of each node',
+  link: '/technologies_graph',
+  icon: <BarChartIcon/>
+},{
+  title: 'BC Economic Map',
+  subHeader: 'An interactive map of BC',
+  info: 'Click an economic region to generate a randomize pie chart, real data not included',
+  link: '/bc_economic_map',
+  icon: <MapIcon/>
+},{
+  title: 'Canada Polygon Map',
+  subHeader: 'A basic map with an on hover effect',
+  info: '',
+  link: '/canadian_polygon_map',
+  icon: <MapIcon/>
+},{
+  title: 'Technology Tags',
+  subHeader: 'an interactive tag map',
+  info: 'Most Popular Tags @ Stack Overflow, tags link to Stack Overflow',
+  link: '/tech_tags_graph',
+  icon: <BarChartIcon/>
+}
+]
+function AppletsList() {
   const classes = useStyles();
 
   return (
-    <List className={classes.root}>
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="" src="" />
-        </ListItemAvatar>
-        <ListItemText
-          primary='title'
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                subHeader
-              </Typography>
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="" src="" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="title"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                subHeader
-              </Typography>
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="" src="" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="title"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                subHeader
-              </Typography>
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-    </List>
+    <Paper>
+      <List className={classes.root}>
+        {applets.map( (applet, index) => { return (
+          <>
+            <ListItem alignItems="flex-start" component={RouterLink} to={applet.link}>
+              <ListItemAvatar>
+                <Avatar>
+                  {applet.icon}
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={applet.title}
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      className={classes.inline}
+                      color="textPrimary"
+                      >
+                      {applet.subHeader}
+                    </Typography>
+                  </React.Fragment>
+                }
+                />
+            </ListItem>
+            {(applets.length !== index + 1) && <Divider variant="inset" component="li" />}
+          </>
+        )})}
+      </List>
+    </Paper>
   );
 }
 
@@ -105,8 +109,8 @@ const HomePage = () => {
       <Scrollbar
         style={{ height: '100%', width: '100%', display: 'flex', flex: 1 }}
       >
-        <AlignItemsList/>
-        {intl.formatMessage({ id: 'home' })}
+        <AppletsList/>
+        {/* {intl.formatMessage({ id: 'home' })} */}
       </Scrollbar>
     </Page>
   )
