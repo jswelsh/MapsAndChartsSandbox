5f98a0a49097cc999d4dfa0beb455ca785bcdf91
jswelsh <james.sinclair.welsh@gmail.com>
2020-12-25 10:54:11 -0800

    adding locations for weather map

--- src/pages/CanadianPolygonMap/index.tsx
+++ src/pages/CanadianPolygonMap/index.tsx
@@ -20,7 +20,6 @@ const useStyles = makeStyles(() => ({
   },
 }))
 
-
 const CanadianPolygonMap = () => {
   const intl = useIntl()
   const classes = useStyles()
--- src/pages/Weather/index.js
+++ src/pages/Weather/index.js
@@ -11,7 +11,7 @@ import { useIntl } from 'react-intl'
 import Page from 'material-ui-shell/lib/containers/Page'
 // @ts-ignore
 import Scrollbar from 'material-ui-shell/lib/components/Scrollbar'
-
+import * as data from "./data.json"
 
 const useStyles = makeStyles(() => ({
   Weather: {
@@ -29,28 +29,21 @@ const Weather = () => {
   useEffect(() => {
 am4core.useTheme(am4themes_dark);
 am4core.useTheme(am4themes_animated);
-// Themes end
 
-// Create map instance
 let chart = am4core.create("chartDiv", am4maps.MapChart);
 
-// Set map definition
 chart.geodata = am4geodata_worldHigh;
 
 chart.projection = new am4maps.projections.Mercator();
-
-// Center on the groups by default
 chart.homeZoomLevel = 30;
 chart.homeGeoPoint = { longitude: -122.8, latitude: 52};
-// 49.199797252005844, -122.81153857119284
-// Polygon series
+
 let polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
 polygonSeries.exclude = ["AQ"];
 polygonSeries.useGeodata = true;
 polygonSeries.nonScalingStroke = true;
 polygonSeries.strokeOpacity = 0.5;
 
-// Image series
 let imageSeries = chart.series.push(new am4maps.MapImageSeries());
 let imageTemplate = imageSeries.mapImages.template;
 imageTemplate.propertyFields.longitude = "longitude";
@@ -70,56 +63,7 @@ label.horizontalCenter = "middle";
 label.verticalCenter = "top";
 label.dy = 20;
 
-imageSeries.data = [{
-  "latitude": 40.416775,
-  "longitude": -3.703790,
-  "imageURL": "https://www.amcharts.com/lib/images/weather/animated/rainy-1.svg",
-  "width": 32,
-  "height": 32,
-  "label": "Madrid: +22C"
-}, {
-  "latitude": 48.856614,
-  "longitude": 2.352222,
-  "imageURL": "https://www.amcharts.com/lib/images/weather/animated/thunder.svg",
-  "width": 32,
-  "height": 32,
-  "label": "Paris: +18C"
-}, {
-  "latitude": 52.520007,
-  "longitude": 13.404954,
-  "imageURL": "https://www.amcharts.com/lib/images/weather/animated/cloudy-day-1.svg",
-  "width": 32,
-  "height": 32,
-  "label": "Berlin: +13C"
-}, {
-  "latitude": 52.229676,
-  "longitude": 21.012229,
-  "imageURL": "https://www.amcharts.com/lib/images/weather/animated/day.svg",
-  "width": 32,
-  "height": 32,
-  "label": "Warsaw: +22C"
-}, {
-  "latitude": 41.872389,
-  "longitude": 12.480180,
-  "imageURL": "https://www.amcharts.com/lib/images/weather/animated/day.svg",
-  "width": 32,
-  "height": 32,
-  "label": "Rome: +29C"
-}, {
-  "latitude": 51.507351,
-  "longitude": -0.127758,
-  "imageURL": "https://www.amcharts.com/lib/images/weather/animated/rainy-7.svg",
-  "width": 32,
-  "height": 32,
-  "label": "London: +10C"
-}, {
-  "latitude": 59.329323,
-  "longitude": 18.068581,
-  "imageURL": "https://www.amcharts.com/lib/images/weather/animated/rainy-1.svg",
-  "width": 32,
-  "height": 32,
-  "label": "Stockholm: +8C"
-} ];
+imageSeries.data = data.nodes;
 }, [])
 return ( 
   <Page
@@ -135,3 +79,4 @@ return (
 }
 
 export default Weather
+// https://www.amcharts.com/lib/images/weather/animated/thunder.svg
