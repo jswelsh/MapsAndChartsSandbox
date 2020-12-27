decbad5e28e9479d51c370e2bee65e8c8e0cb36c
jswelsh <james.sinclair.welsh@gmail.com>
2020-12-27 10:37:43 -0800

    add functionality to select region

--- src/pages/Weather/index.js
+++ src/pages/Weather/index.js
@@ -1,5 +1,5 @@
 /* Imports */
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
 import { makeStyles } from '@material-ui/core/styles'
@@ -110,84 +110,151 @@ const iconMap = {
   803: 'cloudy.svg',
   804: 'cloudy.svg',
 }
-const alberta = [
-  { name: 'Jasper', id: 5985918 },
-  { name: 'Edmonton', id: 5946768 },
-  { name: 'Red Deer', id: 6118158 },
-  { name: 'Calgary', id: 5913490 },
-  { name: 'Medicine Hat', id: 6071618 },
-  { name: 'Fort McMurray', id: 5955895 },
-  { name: 'Grande Prairie', id: 5964347 }
-]
+const CAN = {
+  BC:[
+    { name: 'Vancouver', id: 6173331 },
+    { name: 'Victoria', id: 6174041 },
+    { name: 'kelowna', id: 5990579 },
+    { name: 'kamloops', id: 5989045 },
+    { name: 'williams lake', id: 6182212 },
+    { name: 'Prince Rupert', id: 6113406 },
+    { name: 'Courtenay', id: 5930890 },
+    { name: 'Port Hardy', id: 6111862 },
+    { name: 'Prince George', id: 6113365 },
+    { name: 'Bella Bella', id: 5897730 },
+    { name: 'Clearwater', id: 5923667 },
+    { name: 'Smithers', id: 6149996 },
+    { name: 'Terrace', id: 6162949 }],
+  AB:[
+    { name: 'Jasper', id: 5985918 },
+    { name: 'Edmonton', id: 5946768 },
+    { name: 'Red Deer', id: 6118158 },
+    { name: 'Calgary', id: 5913490 },
+    { name: 'Medicine Hat', id: 6071618 },
+    { name: 'Fort McMurray', id: 5955895 },
+    { name: 'Grande Prairie', id: 5964347 }],
+  SK: [
+    { name: 'Saskatoon', id: 6141256 },
+    { name: 'Regina', id: 6119109 },
+    { name: 'Prince Albert', id: 6113335 }],
+  MB: [
+    { name: 'Winnipeg', id: 6183235 },
+    { name: 'Brandon', id: 5907896 }],
+  ON: [
+    // { name: 'Kitchener', id: 5992996 },
+    // { name: 'London', id: 6058560 },
+    // { name: 'Hamilton', id: 5969782 },
+    // { name: 'Sault Ste. Marie', id: 6141439 },
+    // { name: 'Barrie', id: 5894171 },
+    { name: 'Moosonee', id: 6078372 },
+    { name: 'Toronto', id: 6167865 },
+    { name: 'Ottawa', id: 6094817 },
+    { name: 'Kenora', id: 5991055 },
+    { name: 'Sudbury', id: 5964700 },
+    { name: 'Thunder Bay', id: 6166142 }],
+  QC: [
+    // { name: 'Quebec City', id:  },
+    // { name: 'Sherbrooke', id: 7284311 }
+    { name: 'Montreal', id: 6077243 },
+    { name: 'Saguenay', id: 6137270 },
+    { name: "Val-d'Or", id: 6173017 },  ],
+  NB: [
+    // { name: 'Moncton', id: 6076211 }
+    { name: 'Saint John', id: 6138517 },
+    { name: 'Fredericton', id: 5957776 }],
+  NS: [
+    { name: 'Halifax', id: 6324729 },
+    { name: 'Sydney', id: 6354908 },
+    { name: 'Truro', id: 6169587 }],
+  PEI: [
+    // { name: 'Summerside', id: 6159244 }
+    { name: 'Charlottetown', id: 5920288 }],
+  NL: [
+    // { name: 'Grand Falls-Windsor', id: 5964378 },
+    { name: "St. John's", id: 6324733 },
+    { name: 'Gander', id: 5959335 },
+    { name: 'Corner Brook', id: 5927969 }], 
+}
+const regions = {
+  westCoast: {
+    cities: [...CAN.BC],
+    zoom: 20,
+    coords: {
+      longitude: -123,
+      latitude: 51.5
+    }
+  },
+  prairies: {
+    cities: [...CAN.AB,...CAN.SK,...CAN.MB],
+    zoom: 15,
+    coords: {
+      longitude: -108.9,
+      latitude: 53
+    }
+  },
+  central: {
+    cities: [...CAN.QC,...CAN.ON],
+    zoom: 15,
+    coords: {
+      longitude: -82,
+      latitude: 46.8
+    }
+  },
+  atlantic: {
+    cities: [...CAN.NB,...CAN.NS,...CAN.PEI,...CAN.NL],
+    zoom: 20,
+    coords: {
+      longitude:  -60,
+      latitude: 47.2,
+    }
+  }
+}
 
-const saskatchewan = [
-  { name: 'Saskatoon', id: 6141256 },
-  { name: 'Regina', id: 6119109 },
-  { name: 'Prince Albert', id: 6113335 }
-]
 
-const manitoba = [
-  { name: 'Winnipeg', id: 6183235 },
-  { name: 'Brandon', id: 5907896 }
-]
-
-const ontario = [
-  { name: 'Toronto', id: 6167865 },
-  { name: 'Ottawa', id: 6094817 },
-  // { name: 'Hamilton', id: 5969782 },
-  // { name: 'Kitchener', id: 5992996 },
-  { name: 'London', id: 6058560 },
-  { name: 'Kenora', id: 5991055 },
-  { name: 'Thunder Bay', id: 6166142 },
-  // { name: 'Sault Ste. Marie', id: 6141439 },
-  { name: 'Sudbury', id: 5964700 },
-  { name: 'Barrie', id: 5894171 },
-
-]
-
-/* kelowna,kamloops,williams lake,Prince Rupert,Revelstoke,Courtenay,Port Hardy, Prince George,Bella Bella, Clearwater,Smithers,Terrace,Seattle,Spokane,Portland */
-const ids = '6173331,6180144,5976783,6174041,5990579,5989045,6182212,6113406,6121621,5930890,6111862,6113365,5897730,5923667,6149996,6162949,5809844,5811696,5746545'
-// const apiKey = '247caa6dbdb7d1f3c1bf6aa5fac887ed'
-let weatherReports = []
 const Weather = () => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-const intl = useIntl()
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const [reports, setReports] = useState([])
+  const [selectedRegion, setSelectedRegion] = useState('westCoast')
+
+
+  const intl = useIntl()
   const classes = useStyles()
+
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+    setAnchorEl(event.currentTarget)
   };
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleClose = (region) => {
+    setSelectedRegion(region)
+    setAnchorEl(null)
   };
+    useEffect(() => {
+      axios
+      .get(`http://api.openweathermap.org/data/2.5/group?id=${(regions[selectedRegion].cities.map(city => city.id)).join()}&units=metric&appid=${process.env.REACT_APP_WEATHER_ACCESS_TOKEN}`)
+      .then(res => res.data.list)
+      .then(weatherLists => weatherLists.map((report, index) => {return {
+        latitude: report.coord.lat,
+        longitude: report.coord.lon,
+        imageURL: `https://www.amcharts.com/lib/images/weather/animated/${iconMap[report.weather[0].id]}`,
+        width: 32,
+        height: 32,
+        label: `${report.name}: ${report.main.temp}°C`
+      }}))
+      .then(res => setReports(res))
+    }, [selectedRegion])
 
-  /* ${process.env.REACT_APP_WEATHER_ACCESS_TOKEN} */
   useEffect(() => {
-    axios
-    .get(`http://api.openweathermap.org/data/2.5/group?id=${(ontario.map(city => city.id)).join()}&units=metric&appid=${process.env.REACT_APP_WEATHER_ACCESS_TOKEN}`)
-    .then((res) => res.data.list)
-    .then((weatherLists) => weatherLists.map((report, index) => {return {
-      latitude: report.coord.lat, 
-      longitude: report.coord.lon,/* ${iconMap[report.weather[0].id]} */
-      imageURL: `https://www.amcharts.com/lib/images/weather/animated/${iconMap[report.weather[0].id]}`,
-      width: 32,
-      height: 32,
-      label: `${report.name}: ${report.main.temp}°C`
-    }}))
-    .then((res) => {
-      console.log(res)
-      weatherReports = [...res]})
-      .then((res) => {
-
     am4core.useTheme(am4themes_dark);
     am4core.useTheme(am4themes_animated);
 
     let chart = am4core.create("chartDiv", am4maps.MapChart);
 
     chart.geodata = am4geodata_worldHigh;
-
-    chart.projection = new am4maps.projections.Mercator();
-    chart.homeZoomLevel = 25;
-    chart.homeGeoPoint = { longitude: -122.8, latitude: 50};
+    chart.projection = new am4maps.projections.Mercator()
+    chart.homeZoomLevel = regions[selectedRegion].zoom
+    chart.homeGeoPoint = { 
+      longitude: regions[selectedRegion].coords.longitude,
+      latitude: regions[selectedRegion].coords.latitude
+    }
 
     let polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
     polygonSeries.exclude = ["AQ"];
@@ -213,14 +280,12 @@ const intl = useIntl()
     label.horizontalCenter = "middle";
     label.verticalCenter = "top";
     label.dy = 20;
-
-    imageSeries.data = weatherReports;
+// console.log(getWeatherReports(CAN.BC));
+    imageSeries.data = reports;
     return () => {
       chart.dispose()
       }
-    })
-    /* weatherReports */
-  }, [])
+  }, [reports])
 return ( 
   <Page
     pageTitle={intl.formatMessage({
@@ -239,13 +304,15 @@ return (
         open={Boolean(anchorEl)}
         onClose={handleClose}
         >
-        <MenuItem onClick={handleClose}>WestCoast</MenuItem>
-        <MenuItem onClick={handleClose}>Prairies</MenuItem>
-        <MenuItem onClick={handleClose}></MenuItem>
+        <MenuItem onClick={() => handleClose('westCoast')}>WestCoast</MenuItem>
+        <MenuItem onClick={() => handleClose('prairies')}>Prairies</MenuItem>
+        <MenuItem onClick={() => handleClose('central')}>Central</MenuItem>
+        <MenuItem onClick={() => handleClose('atlantic')}>Atlantic</MenuItem>
       </Menu>
     </Scrollbar>
   </Page>
 )
 }
+
 
 export default Weather
